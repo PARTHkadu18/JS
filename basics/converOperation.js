64b81b27f8a49b0001554c3ff9c49e412e56bce1
PARTHkadu18 <150957628+PARTHkadu18@users.noreply.github.com>
2024-12-28 17:01:17 +0000

    bj

--- basics/converOperation.js
+++ basics/converOperation.js
@@ -3,3 +3,8 @@
 // "33" =>33
 // "asd" or "23f" or undefined =>NaN
 // null => 0
+
+console.log(2 + "3");
+console.log("2" + 3);
+console.log(2 + 2+ "3");
+console.log("2" + 2+ 3);
---
+++ basics/dataType.js
@@ -0,0 +1,6 @@
+// Primitive
+// 7 types: 
+// String, number, boolena, null, undefined, Symbol, BogInt
+
+// Non Primitive OR Reference type
+// Arrays, objects, functions
