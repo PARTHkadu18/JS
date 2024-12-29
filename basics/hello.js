8dcc4c2241e60fd23ad206589a65504a1420e3f7
PARTHkadu18 <150957628+PARTHkadu18@users.noreply.github.com>
2024-12-29 13:05:41 +0000

    com

--- basics/hello.js
+++ basics/hello.js
@@ -1,4 +1,7 @@
-let score="a"
-let xyz=Number(score)
-console.log(typeof xyz)
-console.log(xyz)
+// let score="a"
+// let xyz=Number(score)
+// console.log(typeof xyz)
+// console.log(xyz)
+
+
+console.log(Math.round(Math.random()*10));
---
+++ basics/string.js
@@ -0,0 +1,17 @@
+const name="Parth"
+
+const lname=new String("Kadu")
+
+const fname=`My name is ${name} and my last name is ${lname}`
+
+// console.log(fname);
+
+const s="ParthKadu"
+
+s1=s.substring(1,4)
+console.log(s1);
+
+s2=s.slice(-7,4)
+console.log(s2);
+
+//replace
